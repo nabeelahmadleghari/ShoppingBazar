2d2410c8e81eb4567c4fc1685f3bb51a25fcfd37
nabeelahmadleghari <nabeelleghari20@gmail.com>
2021-12-22 23:42:34 +0500

    CRUD operationsin One Page using Ajax with Asp.Net MVC

---
+++ ShoppingBazar.Services/ProductService.cs
@@ -0,0 +1,72 @@
+﻿using soppingbazar.Entities;
+using ShoppingBazar.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBazar.Services
+{
+    public class ProductService
+    {
+
+        
+        public product GetProducts(int ID) //category id k through get krne k liay
+        {
+            using (var context = new SBcontext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+
+        //*************************************
+
+        public List<product> GetProducts() //database se list of categories dekhne k liay
+        {
+            using (var context = new SBcontext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        //*************************************************
+        public void SaveProduct(product Product) //Database me categories add krne k liay
+        {
+            using (var context = new SBcontext())
+            {
+                context.Products.Add(Product);
+                context.SaveChanges();
+            }
+
+
+        }
+
+
+        public void UpdateProduct(product Product) //Database me categories update krne k liay
+        {
+            using (var context = new SBcontext())
+            {
+                context.Entry(Product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+
+
+        }
+
+
+
+
+        public void DeleteCategory(int ID) //Database me categories Delete krne k liay
+        {
+            using (var context = new SBcontext())
+            {
+                var Product = context.Products.Find(ID);
+                context.Entry(Product).State = System.Data.Entity.EntityState.Deleted;
+                context.SaveChanges();
+            }
+
+
+        }
+    }
+}
---
+++ shoppingbazar.web/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using ShoppingBazar.Services;
+using soppingbazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shoppingbazar.web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductService serviceproduct = new ProductService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ProductTable(string search)
+        {
+            var products = serviceproduct.GetProducts();
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                products = products.Where(p => p.Name.ToLower().Contains(search.ToLower())).ToList();
+            }
+           
+            return PartialView(products);
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(product Product)
+        {
+
+            serviceproduct.SaveProduct(Product);
+            return RedirectToAction("ProductTable");
+        }
+    }
+}
